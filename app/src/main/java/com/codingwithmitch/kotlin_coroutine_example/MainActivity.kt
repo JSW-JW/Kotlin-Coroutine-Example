f7aa33701c4a33ceed43f3151df30b24eadf6487
Seungwon <jshtmddnjs@naver.com>
2020-11-06 11:10:16 +0900

    coroutine scope (IO, Main)

---
+++ app/src/main/java/com/codingwithmitch/kotlin_coroutine_example/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.codingwithmitch.kotlin_coroutine_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+
+class MainActivity : AppCompatActivity() {
+    private val RESULT_1 = "RESULT #1"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+           CoroutineScope(IO).launch { // CoroutineScope extends CoroutineBuilder
+               val data = fakeApiRequest()
+
+               withContext(Main) {
+                   text.text = data
+               }
+           }
+        }
+    }
+
+    private suspend fun fakeApiRequest() : String {
+        val result1 = getResult1FromApi()
+        println("DEBUG : ${result1}")
+        return result1
+    }
+
+    private suspend fun getResult1FromApi(): String {
+        logThread("getResult1FromApi")
+        delay(1000)
+        /* This is very different from 'Thread.sleep(1000)'. It makes the Thread itself sleep, so that it will make all coroutines in the thread also sleep.
+        But, delay() makes only the coroutine sleep. */
+        return RESULT_1
+    }
+
+
+    private fun logThread(methodName: String) {
+        println("DEBUG: ${methodName}: ${Thread.currentThread().name}")
+    }
+}
