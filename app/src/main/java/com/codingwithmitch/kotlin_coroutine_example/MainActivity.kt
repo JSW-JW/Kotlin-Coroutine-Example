bb90490ebe0b1e286b3c7d1c695d60c8e4c5d962
Seungwon <jshtmddnjs@naver.com>
2020-11-06 16:41:55 +0900

    Completable-Job-job.cancel

--- app/src/main/java/com/codingwithmitch/kotlin_coroutine_example/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/kotlin_coroutine_example/MainActivity.kt
@@ -2,68 +2,94 @@ package com.codingwithmitch.kotlin_coroutine_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
-    private val RESULT_1 = "RESULT #1"
-    private val RESULT_2 = "RESULT #2"
+
+    private val PROGRESS_MAX = 100
+    private val PROGRESS_START = 0
+    private val JOB_TIME = 4000 // ms
+
+    private lateinit var job: CompletableJob
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-           CoroutineScope(IO).launch { // CoroutineScope extends CoroutineBuilder
-               val response1 = fakeApiRequest1()
-               executeFunctionOnMainThread { setTextView(response1) }
+        job_button.setOnClickListener {
+            if (!::job.isInitialized) {
+                job = Job()
+            }
+            job_progress_bar.startJobOrCancel(job)
+        }
 
-               val response2 = fakeApiRequest2(response1)
-               executeFunctionOnMainThread { setTextView(response2) }
-           }
+
+    }
+
+
+    private fun initJob() {
+        job_button.text = "Start Job #1"
+        job_progress_bar.progress = 0
+        updateJobCompleteTextView("")
+        job = Job()
+        job.invokeOnCompletion {
+            it?.message.let {
+                var msg = it
+                if (it.isNullOrBlank()) {
+                    msg = "Unknown cancellation error."
+                }
+                println("${job} was cancelled. Reason: $msg")
+                showToast(msg)
+            }
+        }
+
+    }
+
+    private fun ProgressBar.startJobOrCancel(aJob: Job) {
+        if (this.progress > 0) {
+            resetJob(aJob)
+        } else {
+            job_button.text = "Cancel Job #1"
+            CoroutineScope(IO + aJob).launch {
+                for (i in PROGRESS_START..PROGRESS_MAX) {
+                    delay(JOB_TIME / PROGRESS_MAX.toLong())
+                    this@startJobOrCancel.progress = i
+                }
+                withContext(Main) {
+                    updateJobCompleteTextView("Job is complete!")
+                    this@startJobOrCancel.progress = 0
+                    resetJob(aJob)
+                    job_progress_bar.startJobOrCancel(job)
+
+                }
+            }
+
+
         }
     }
 
-    private suspend fun executeFunctionOnMainThread(myFunction : () -> Unit) {
-        withContext(Main) {
-            myFunction()
+    private fun resetJob(job: Job) {
+        if (job.isActive || job.isCompleted) {
+            job.cancel(CancellationException("Resetting job"))
+        }
+        initJob()
+    }
+
+    private fun updateJobCompleteTextView(text: String) {
+        GlobalScope.launch(Main) {
+            job_complete_text.text = text
         }
     }
 
-    private fun setTextView(newText: String) {
-        text.text = newText
+    private fun showToast(text: String?) {
+        GlobalScope.launch(Main) {
+            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+        }
     }
 
-    private suspend fun fakeApiRequest1() : String {
-        val result1 = getResult1FromApi()
-        println("DEBUG : ${result1}")
-        return result1
-    }
-
-    private suspend fun fakeApiRequest2(result1: String) : String {
-        val result1 = getResult2FromApi(result1)
-        println("DEBUG : ${result1}")
-        return result1
-    }
-
-    private suspend fun getResult1FromApi(): String {
-        logThread("getResult1FromApi")
-        delay(1000)
-        /* This is very different from 'Thread.sleep(1000)'. It makes the Thread itself sleep, so that it will make all coroutines in the thread also sleep.
-        But, delay() makes only the coroutine sleep. */
-        return RESULT_1
-    }
-
-    private suspend fun getResult2FromApi(result1: String): String {
-        logThread("getResult2FromApi")
-        delay(1000)
-        return RESULT_2
-    }
-
-
-    private fun logThread(methodName: String) {
-        println("DEBUG: ${methodName}: ${Thread.currentThread().name}")
-    }
 }
