3a213d573b5fcb074376a95642672e47d187062d
Seungwon <jshtmddnjs@naver.com>
2020-11-06 13:40:34 +0900

    request1 result1 request2(result1)

--- app/src/main/java/com/codingwithmitch/kotlin_coroutine_example/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/kotlin_coroutine_example/MainActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
     private val RESULT_1 = "RESULT #1"
+    private val RESULT_2 = "RESULT #2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +17,11 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
            CoroutineScope(IO).launch { // CoroutineScope extends CoroutineBuilder
-               val response = fakeApiRequest()
-               executeFunctionOnMainThread { setTextView(response) }
+               val response1 = fakeApiRequest1()
+               executeFunctionOnMainThread { setTextView(response1) }
+
+               val response2 = fakeApiRequest2(response1)
+               executeFunctionOnMainThread { setTextView(response2) }
            }
         }
     }
@@ -32,8 +36,14 @@ class MainActivity : AppCompatActivity() {
         text.text = newText
     }
 
-    private suspend fun fakeApiRequest() : String {
+    private suspend fun fakeApiRequest1() : String {
         val result1 = getResult1FromApi()
+        println("DEBUG : ${result1}")
+        return result1
+    }
+
+    private suspend fun fakeApiRequest2(result1: String) : String {
+        val result1 = getResult2FromApi(result1)
         println("DEBUG : ${result1}")
         return result1
     }
@@ -44,6 +54,12 @@ class MainActivity : AppCompatActivity() {
         /* This is very different from 'Thread.sleep(1000)'. It makes the Thread itself sleep, so that it will make all coroutines in the thread also sleep.
         But, delay() makes only the coroutine sleep. */
         return RESULT_1
+    }
+
+    private suspend fun getResult2FromApi(result1: String): String {
+        logThread("getResult2FromApi")
+        delay(1000)
+        return RESULT_2
     }
 
 
